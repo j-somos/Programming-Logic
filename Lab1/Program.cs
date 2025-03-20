0237b71cc24b1cf9fdc2f44cb106ecc472a2400e
j-somos <jsomos@protonmail.com>
2025-03-20 22:52:05 +0000

    Completed Lab 1 Items 1-6

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Lab1;
+﻿
+using System.Xml.Serialization;
+
+namespace Lab1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Welcome to IT-1050!");
+        // Step1
+        Console.WriteLine("Jacqueline Somos");
+        Console.WriteLine("IT 1050 - Programming Logic");
+
+        // Step2
+        /* 
+        Jacqueline Somos 
+        IT-1050-Lab1 
+        */
+        // Step3
+       
+        int temp = 75;
+        Console.WriteLine("The current temperature is " + temp + " degrees.");
+       
+        string fav_lang = "Python";
+        Console.WriteLine("My favorite computer language is " + fav_lang + ".");
+       
+        double prog_count = 1.8e38;
+        Console.WriteLine("The number of programs I've written is " + prog_count + ".");
+       
+        bool myanswer = true;
+        Console.WriteLine("My answer is "+ myanswer + " to having programming experience");
+
+         // Step5
+
+         const string mschool = "Tri-C";
+         Console.WriteLine("My school is "+ mschool);
     }
-}
+       
+    
+}   
--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcd98207080b5e3077e880dac893a5ceafc458d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6532d4836111f8d061a5c9a7d5378ec4f2c8088a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
